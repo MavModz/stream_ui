9a445995402e8c226291a3f730ff1039cf004736
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-19 18:38:49 +0530

    Login form

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,28 +1,95 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import React from 'react';
-import {auth,provider} from "../../firebase";
+import { auth, provider } from "../../firebase";
 import { signInWithPopup } from 'firebase/auth';
 import Dashboard from "../Dashboard/Dashboard";
+import logo from "../../Components/assets/logo.png";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import GoogleButton from 'react-google-button';
+import googleicon from "../../Components/assets/google.svg"
 
-const Login =() => {
-    const [email , setEmail]  =useState('');
+const Login = () => {
+    const [email, setEmail] = useState('');
     const handleclick = () => {
-        signInWithPopup(auth,provider).then((data)=>{
+        signInWithPopup(auth, provider).then((data) => {
             setEmail(data.user.email);
             sessionStorage.setItem('email', data.user.email);
-            
+
         });
     }
-        useEffect(() =>{
-            sessionStorage.getItem('email');
-            
-        },[setEmail]);
-   
-  return (
-    <>
-    {email? <Dashboard /> :<input type="button" value="SignWithGoogle" onClick={handleclick}/>}
-    </>
-  )
+    useEffect(() => {
+        sessionStorage.getItem('email');
+
+    }, [setEmail]);
+
+    return (
+        <>
+            {/* Header Start */}
+            <div className="container">
+                <div className="header">
+                    {/* Logo Start  */}
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    {/* Logo End  */}
+
+                    {/* Menubar start  */}
+                    <div className="menubar">
+                        <ul className="navbar">
+                            <li className="menulist">
+
+                                <Link to="/" className="lists">Home</Link>
+                            </li>
+                            <li className="menulist">
+
+                                <Link to="/" className="lists">Join</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* menubar end  */}
+                </div>
+                {/* Body Start  */}
+
+                <div className="payloader">
+                    <div className="content">
+                        <div className="heading">
+                            <h1>Hello Designer</h1><br />
+                            <p >Welcome to Streaming App</p>
+                        </div>
+                        <div className="field">
+                            <div className="emailid">
+                                <input type="email" name="email" id="email" placeholder="Email" />
+                            </div>
+                            <div className="password">
+                                <input type="password" name="password" id="password" placeholder="Password" />
+                            </div>
+                            <div className="forgot">
+                                <Link to="/forgot" id="forgotp">Forgot Password?</Link>
+                            </div>
+                            <div className="or">
+                                <p>or</p>
+                            </div>
+                            <div className="login-with-google">
+                                {email ? <Dashboard /> :
+                                    <GoogleButton onClick={handleclick} id="google"/>}
+                            </div>
+                            <div className="login-button">
+                                <input type="submit" value="Login" name="submit"/>
+                            </div>
+                            
+                            <div className="singin">
+                                Don't have an account?{" "}<Link to="/singin">Sign Up Now!</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Body End  */}
+            </div>
+
+
+        </>
+    )
 }
 
 export default Login
