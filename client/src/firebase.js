65b9cb3625178b2dd3d5f0af80688bff2750c44d
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-19 15:42:59 +0530

    firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf-nc6N3DkOaqAREV-i36EvHTbLWOWjkQ",
+  authDomain: "streamui-fdd4f.firebaseapp.com",
+  projectId: "streamui-fdd4f",
+  storageBucket: "streamui-fdd4f.appspot.com",
+  messagingSenderId: "493975566599",
+  appId: "1:493975566599:web:badf499d60b3c217092da2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
