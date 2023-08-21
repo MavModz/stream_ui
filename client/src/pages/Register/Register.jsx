a725d68b66217047c18bc30d3d799a94e65d36db
mavmodz <maverickjetman07@gmail.com>
2023-08-21 17:35:34 +0530

    minor ui updates

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -4,24 +4,25 @@ import "./Register.css"
 function Register() {
     return (
         <div className="register-container">
-            <h1>Register</h1>
             <div className="register-wrapper">
                 <div className="left-wrapper">
+                    <span>Start For Free</span>
+                    <h1>Create a New Account</h1>
+                    <span>Already a Member</span>
                     <form className='form-wrapper'>
                         <div className="text-field">
-                            <input type="text" name="fname" id="fname" placeholder='First Name'/>
-                            <input type="text" name="lname" id="lname" placeholder='Last Name'/>
+                            <input type="text" name="fname" id="fna
+                            
+                            me" placeholder='First Name' />
+                            <input type="text" name="lname" id="lname" placeholder='Last Name' />
                         </div>
                         <div className="text-field">
-                            <input type="email" name="email" id="email" placeholder='Email'/>
+                            <input type="email" name="email" id="email" placeholder='Email' />
                         </div>
                         <div className="text-field">
-                            <input type="password" name="password" id="password" placeholder='Password'/>
+                            <input type="password" name="password" id="password" placeholder='Password' />
                         </div>
                     </form>
-                </div>
-                <div className="right-wrapper">
-                    <p>testing right</p>
                 </div>
             </div>
         </div>
